a94314c5d4ff1aaddabdcdbffc36ce944c52d3a7
Mital Thummar <ing.thummar@yahoo.com>
2024-09-07 21:28:33 -0400

    Code Improvement
    Refactored code to create reusable components

--- FileUploadApp/Data/FileRecord.cs
+++ FileUploadApp/Data/FileRecord.cs
@@ -12,23 +12,17 @@ public class FileRecord
     /// <summary>
     /// Gets or sets the name of the file.
     /// </summary>
-    [Required]
-    [StringLength(255, ErrorMessage = "File name cannot exceed 255 characters.")]
-    public required string Name { get; set; }
+    public string Name { get; set; }
 
     /// <summary>
     /// Gets or sets the path where the file is stored.
     /// </summary>
-    [Required]
-    [StringLength(1000, ErrorMessage = "File path cannot exceed 1000 characters.")]
-    public required string Path { get; set; }
+    public string Path { get; set; }
 
     /// <summary>
     /// Gets or sets the extension of the file.
     /// </summary>
-    [Required]
-    [StringLength(10, ErrorMessage = "File extension cannot exceed 10 characters.")]
-    public required string Extension { get; set; }
+    public string Extension { get; set; }
 
     /// <summary>
     /// Gets or sets the date and time when the file was created.
---
+++ FileUploadApp/Shared/FUGrid.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace FileUploadApp.Shared
+{
+    public partial class FUGrid
+    {
+        [Parameter]
+        public IList<FileUploadModel> FileUploadModels { get; set; }
+
+        [Parameter]
+        public RenderFragment? Columns { get; set; }
+    }
+}
+
